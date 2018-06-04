4cfa9771a7b83b7adb0784ce108f247999de05e5
Gautier Fauchart <gautier.fauchart@gmail.com>
2018-06-04 23:57:25 +0100

    add images to login pages and remove filter for logout users

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  providerImages = [
+    './assets/providerImages/asos.png',
+    './assets/providerImages/uberEATS.png',
+    './assets/providerImages/jsutEat.png',
+    './assets/providerImages/PizzaHut.png',
+    './assets/providerImages/amazon.png'
+  ]
+
   login() {
     gapi.auth2.getAuthInstance().signIn();
   }
